708d49925bb4bcf9f42ad0347b1da7803f18100a
nathan hains <nathanhains14@yahoo.com>
2021-05-12 10:47:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+
+function submitData(userName, userEmail){
+    fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name: userName,
+        email: userEmail
+    })
+    })
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        document.body.innerHTML = object["id"]
+      });
+}
